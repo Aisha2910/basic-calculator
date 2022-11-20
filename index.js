314a5332b3989ee0d7cc7bcad76bef5933b63f7a
Aisha2910 <mustaphaaisha32@gmail.com>
2022-11-20 19:34:08 +0100

    add background color on focus.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let one = document.querySelector('.one');
 let two = document.querySelector('.two');
 let three = document.querySelector('.three');
-let switchBackground = document.querySelector('nav button div')
-let switchContainer = document.querySelector('nav button')
+let toggler = document.querySelector('.toggler')
+let toggleContainer = document.querySelector('.toggle-container')
 let keys = document.querySelectorAll('.button-container button');
 let input = document.querySelector('.input');
 let equal = document.querySelector('.equal-to');
@@ -11,9 +11,9 @@ let i;
 
 one.addEventListener('click',()=>{
     i = 0;
-    switchContainer.classList.add('first')
-    switchContainer.classList.remove('second')
-    switchContainer.classList.remove('third')
+ toggleContainer.classList.add('first')
+ toggleContainer.classList.remove('second')
+ toggleContainer.classList.remove('third')
     for (let theme of themes){
         document.body.classList.remove(theme)
     }
@@ -22,9 +22,9 @@ one.addEventListener('click',()=>{
 })
 two.addEventListener('click',()=>{
     i = 1;
-    switchContainer.classList.add('second')
-    switchContainer.classList.remove('first')
-    switchContainer.classList.remove('third')
+ toggleContainer.classList.add('second')
+ toggleContainer.classList.remove('first')
+ toggleContainer.classList.remove('third')
     for (let theme of themes){
         document.body.classList.remove(theme)
     }
@@ -32,9 +32,9 @@ two.addEventListener('click',()=>{
 })
 three.addEventListener('click',()=>{
     i = 2;
-    switchContainer.classList.add('third')
-    switchContainer.classList.remove('first')
-    switchContainer.classList.remove('second')
+ toggleContainer.classList.add('third')
+ toggleContainer.classList.remove('first')
+ toggleContainer.classList.remove('second')
     for (let theme of themes){
         document.body.classList.remove(theme)
     }
@@ -65,10 +65,7 @@ keys.forEach(key =>{
                  input.textContent = input.textContent.slice(0,input.textContent.length-1) 
                 
                 break;
-            case 'RESET':
-                console.log('reset')
-                
-                break;
+    
             default:
                 input.textContent += (keyValue);
                 
